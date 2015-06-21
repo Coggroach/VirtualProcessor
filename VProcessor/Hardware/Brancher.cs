8bacbe98983491c9cd6cbf722bfd742dc86e8dfb
Coggroach <xpgear2@gmail.com>
2015-06-21 23:17:52 +0100

    Added Brancher and Processor

---
+++ VProcessor/Hardware/Brancher.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VProcessor.Hardware
+{
+    public class Brancher
+    {
+        private Byte Nzcv { get; set; }       
+        public Brancher()
+        {
+            this.Nzcv = 0;
+        }        
+
+        public Boolean Branch(Int32 code)
+        {
+
+            return false;
+        }
+    }
+}
---
+++ VProcessor/Hardware/Processor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VProcessor.Hardware
+{
+    public class Processor
+    {
+        private const Int32 ControlMemorySize = 64;
+        private const Int32 UserMemorySize = 32;
+        private const String ControlMemoryLocation = "Software\\ControlMemory.txt";
+        private const String UserMemoryLocation = "Software\\UserMemory.txt";
+
+        private Datapath datapath;
+        private MemoryUnit controlMemory;
+        private MemoryUnit userMemory;
+        private UInt32 instructionRegister;
+        private Brancher branchControl;
+        public Processor()
+        {
+            this.datapath = new Datapath();
+            this.controlMemory = new MemoryUnit(ControlMemorySize, ControlMemoryLocation);
+            this.userMemory = new MemoryUnit(UserMemorySize, UserMemoryLocation);
+            this.instructionRegister = 0;
+            this.branchControl = new Brancher();
+        }
+
+        public void Tick()
+        {
+            this.instructionRegister = userMemory.GetMemory();
+        }
+
+    }
+}
