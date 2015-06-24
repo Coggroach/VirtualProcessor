e3eb1dcb78549676db3219fdde23f68d3f8dc694
Coggroach <Coggroach@users.noreply.github.com>
2015-06-24 15:54:48 +0100

    Update Brancher.cs
    Started the Magic

--- VProcessor/Hardware/Brancher.cs
+++ VProcessor/Hardware/Brancher.cs
@@ -8,16 +8,55 @@ namespace VProcessor.Hardware
 {
     public class Brancher
     {
-        private Byte Nzcv { get; set; }       
+        public Byte Nzcv { get; set; }       
         public Brancher()
         {
             this.Nzcv = 0;
-        }        
+        }
+        
+        private Boolean BitMatch(Byte bitPos, Byte matchBit)
+        {
+            return (Nzcv >> bitPos) & 1 == matchBit;
+        }
 
         public Boolean Branch(Int32 code)
         {
-
+            //https://www.cs.tcd.ie/John.Waldron/3d1/branches.pdf
+            switch(code)
+            {
+                case Opcode.B:
+                    return true;
+                case Opcode.BEQ:
+                    return BitMatch(2, 1);
+                case Opcode.BNE:
+                    return BitMatch(2, 0);
+                case Opcode.BGT:
+                    return BitMatch(2, 1)*( (BitMatch(3, 1)*BitMatch(0, 1)) + (BitMatch(3, 0)*BitMatch(0, 0)) );
+            }
             return false;
         }
+        
+        private Boolean Parse(String s)
+        {
+            var neg = 1;
+            if(s.Contains("!"))
+                neg = 0;
+            var codes = new[] {"N", "Z", "C", "V"};
+            var i = codes.Where(code => if(s.Contains(code)) { break; } i++);
+            
+            return BitMatch(i, neg);
+        }
+        
+        private static Boolean operator +(Boolean A, Boolean B)
+        {
+            return A || B;    
+        }
+        
+        private static Boolean operator *(Boolean A, Boolean B)
+        {
+            return A && B;
+        }
+        }
+}
     }
 }
