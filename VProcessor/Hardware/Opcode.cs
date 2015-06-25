9b412d19f25430d1421205c0cd85e2bc5803c89c
Coggroach <Coggroach@users.noreply.github.com>
2015-06-25 15:54:53 +0100

    Update Opcode.cs
    Added Branch Codes

--- VProcessor/Hardware/Opcode.cs
+++ VProcessor/Hardware/Opcode.cs
@@ -19,6 +19,22 @@ namespace VProcessor.Hardware
 
         public const Int32 LSL = 10;
         public const Int32 LSR = 11;
+        
+        public const Int32 B   = 0x10000;
+        public const Int32 BEQ = 0x20000;
+        public const Int32 BNE = 0x30000;
+        public const Int32 BCS = 0x40000;
+        public const Int32 BCC = 0x50000;
+        public const Int32 BNS = 0x60000;
+        public const Int32 BNC = 0x70000;
+        public const Int32 BVS = 0x80000;
+        public const Int32 BVC = 0x90000;
+        public const Int32 BHI = 0xA0000;
+        public const Int32 BLS = 0xB0000;
+        public const Int32 BGE = 0xC0000;
+        public const Int32 BLT = 0xD0000;
+        public const Int32 BGT = 0xE0000;
+        public const Int32 BLE = 0xF0000;
 
         private static Hashtable codeTable;
 
@@ -38,6 +54,21 @@ namespace VProcessor.Hardware
             codeTable.Add("DEC", DEC);
             codeTable.Add("LSL", LSL);
             codeTable.Add("LSR", LSR);
+            codeTable.Add("B", B);
+            codeTable.Add("BEQ", BEQ);
+            codeTable.Add("BNE", BNE);
+            codeTable.Add("BCS", BCS);
+            codeTable.Add("BCC", BCC);
+            codeTable.Add("BNS", BNS);
+            codeTable.Add("BNC", BNC);
+            codeTable.Add("BVS", BVS);
+            codeTable.Add("BVC", BVC);
+            codeTable.Add("BHI", BHI);
+            codeTable.Add("BLS", BLS);
+            codeTable.Add("BGE", BGE);
+            codeTable.Add("BLT", BLT);
+            codeTable.Add("BGT", BGT);
+            codeTable.Add("BLE", BLE);
         }
     }
 }
